8262399d15978c037035b5975668e3bb5d120c5c
patrick.qian <patrick.qian@kisio.com>
2022-08-08 15:32:35 +0200

    remove useless commit

--- builder_from_package/run_tyr_beat.sh
+++ builder_from_package/run_tyr_beat.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-exec celery beat -A tyr.tasks
+# we need to wait for the database to be ready
+while ! pg_isready --host=database; do
+    echo "waiting for postgres to be ready"
+    sleep 1;
+done
 
+# export PYTHONPATH=.:../navitiacommon
+
+#db migration
+python /usr/bin/manage_tyr.py db upgrade
+
+exec celery beat -A tyr.tasks
