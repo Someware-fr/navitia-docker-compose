fd48e6f7845f9b3bffd9b1a286be7ca0dbf907bd
patrick.qian <patrick.qian@kisio.com>
2022-08-08 14:58:15 +0200

    multiple fix

--- builder_from_package/run_tyr_web.sh
+++ builder_from_package/run_tyr_web.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# we need to wait for the database to be ready
+while ! pg_isready --host={TYR_DATABASE_HOST}; do
+    echo "waiting for postgres to be ready"
+    sleep 1;
+done
+
 #db migration
 python /usr/bin/manage_tyr.py db upgrade
 echo "MIGRATION IS FINISHED"
